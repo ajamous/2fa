d28d151d537fc787d4e136d80db78b7c2d33fab3
Ameed Jamous <a.jamous@telecomsxchange.com>
2018-10-08 01:46:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
-<?php
 
-/* Send Verification Code
-
-curl -X POST \
-  https://{your_organization}.restcomm.com/2fa/send \
---user  ' {your_account_SID}:{your_account_token}' \
-  -H 'Cache-Control: no-cache' \
-  -H 'Content-Type: application/json' \
-  -d '{
-    "service" : "MyAPP",
-    "from" : "+13216549878",
-    "to" : "+13216549879",
-    "body": "Your verification code is: {code}"
-}
-
-
-*/ 
- ?>
 
 
 <form class="form-group" action="verify.php" method="GET">
